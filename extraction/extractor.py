890f5edfd9747c98dc55ea6a97c585f8ffea97fc
Will Madison <wmadisondev@gmail.com>
2020-04-07 12:05:46 -0400

    testing out intramodule imports.

--- extraction/extractor.py
+++ extraction/extractor.py
@@ -1,15 +1,10 @@
+from extraction.user import User
+
 
 def find_users_by_lastname(conn, last_name):
     cursor = conn.cursor()
     parameters = (last_name,)
     cursor.execute('SELECT * FROM users WHERE last_name = ?', parameters)
     return [User(i[0], i[1], i[2]) for i in cursor.fetchall()]
-
-
-class User:
-    def __init__(self, id, first_name, last_name):
-        self.id = id
-        self.first_name = first_name
-        self.last_name = last_name
 
 
---
+++ extraction/user.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, id, first_name, last_name):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
